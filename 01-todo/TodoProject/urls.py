e578d2939885dd2efc019727c929a56240bd65d8
jtrajano <jlt212000@yahoo.com>
2025-11-23 20:54:19 +0800

    rename project to TodoProject

--- 01-todo/TodoProject/urls.py
+++ 01-todo/TodoProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for WeekOne project.
+URL configuration for TodoProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- 01-todo/TodoProject/wsgi.py
+++ 01-todo/TodoProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WeekOne project.
+WSGI config for TodoProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeekOne.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TodoProject.settings')
 
 application = get_wsgi_application()
